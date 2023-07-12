c435d74cf0da856b41c98480ada410fefc3876f3
Deno234 <denis.ibisi123@gmail.com>
2023-07-12 23:54:19 +0200

    Novi layout
    Postavio sam novi layout, viewBinding, home screen mi je sad postavljen kao fragment te njega pri pokretanju u onCreate odmah pozivam. Trebam dovrsiti layout, te nakon toga postavljam logiku (i preostale ekrane, search bar...) .

---
+++ Projekt/app/src/main/java/com/example/aplikacijazaprognozuvremena/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.aplikacijazaprognozuvremena
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.aplikacijazaprognozuvremena.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
--- Projekt/app/src/test/java/com/example/aplikacijazaprognozuvremena/ExampleUnitTest.kt
+++ Projekt/app/src/test/java/com/example/aplikacijazaprognozuvremena/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.projekt
+package com.example.aplikacijazaprognozuvremena
 
 import org.junit.Test
 
