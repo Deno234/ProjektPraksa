3824f3a28418aa9c97521bfe79fd19f127560c64
Deno234 <denis.ibisi123@gmail.com>
2023-07-28 02:15:53 +0200

    Prikazani podaci na layoutu
    Treba formatirat layout da je prilagoden podacima za free verziju APIja te treba implementirati jos search screen, prilikom rotacije mobitela layout nije optimalan

--- Projekt/app/src/main/java/com/example/aplikacijazaprognozuvremena/homeview/HomeFragmentModel.kt
+++ Projekt/app/src/main/java/com/example/aplikacijazaprognozuvremena/homeview/HomeFragmentModel.kt
@@ -1,5 +1,6 @@
 package com.example.aplikacijazaprognozuvremena.homeview
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.aplikacijazaprognozuvremena.R
 import com.example.aplikacijazaprognozuvremena.network.WeatherData
 import kotlinx.coroutines.launch
+import java.lang.Math.round
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,8 +24,35 @@ class HomeFragmentModel : ViewModel() {
     val weatherData: MutableLiveData<WeatherData?> = _weatherData
     val status: LiveData<WeatherApiStatus> = _status
 
-    private val dateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+    private val _formattedSunrise = MutableLiveData<String>()
+    val formattedSunrise: LiveData<String> = _formattedSunrise
 
+    private val _formattedSunset = MutableLiveData<String>()
+    val formattedSunset: LiveData<String> = _formattedSunset
+
+    private val _formattedHumidity = MutableLiveData<String>()
+    val formattedHumidity: LiveData<String> = _formattedHumidity
+
+    private val _formattedPressure = MutableLiveData<String>()
+    val formattedPressure: LiveData<String> = _formattedPressure
+
+    private val _formattedWindSpeed = MutableLiveData<String>()
+    val formattedWindSpeed: LiveData<String> = _formattedWindSpeed
+
+    private val _currentTemperature = MutableLiveData<String>()
+    val currentTemperature: LiveData<String> = _currentTemperature
+
+    private val _todayMaxTemp = MutableLiveData<String>()
+    val todayMaxTemp: LiveData<String> = _todayMaxTemp
+
+    private val _todayMinTemp = MutableLiveData<String>()
+    val todayMinTemp: LiveData<String> = _todayMinTemp
+
+    private val _currentDateTime = MutableLiveData<String>()
+    val currentDateTime: LiveData<String> = _currentDateTime
+
+    private val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+    private val dateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
     /**
      * Call getWeatherData() on init so we can display data immediately.
      */
@@ -40,11 +69,14 @@ class HomeFragmentModel : ViewModel() {
             _status.value = WeatherApiStatus.LOADING
             try {
                 _weatherData.value = WeatherApi.retrofitService.getWeather()
+                Log.d("HomeFragmentModel", "weatherData: ${_weatherData.value}")
                 _status.value = WeatherApiStatus.DONE
                 updateProperties()
             } catch (e: Exception) {
                 _status.value = WeatherApiStatus.ERROR
                 _weatherData.value = null
+                Log.d("HomeFragmentModel", "weatherDataNULL: ${_weatherData.value}")
+                Log.e("HomeFragmentModel", "$e")
                 clearProperties()
             }
         }
@@ -73,9 +105,25 @@ class HomeFragmentModel : ViewModel() {
         }
     }*/
 
-    private fun updateProperties() {
+    /*private fun updateProperties() {
         weatherData.value?.let { data ->
             weatherData.value = data
+        }
+    }*/
+
+    private fun updateProperties() {
+        weatherData.value?.let { data ->
+            val kelvinToCelsius = {kelvin: Double -> kelvin - 273.15}
+            _currentDateTime.value = dateFormat.format(Date())
+            _formattedSunrise.value = timeFormat.format(Date(data.sys.sunrise * 1000))
+            _formattedSunset.value = timeFormat.format(Date(data.sys.sunset * 1000))
+            _formattedHumidity.value = "${data.main.humidity}%"
+            _formattedPressure.value = "${data.main.pressure} hPa"
+            _formattedWindSpeed.value = "${data.wind.speed} m/s"
+            //_currentTemperature.value = kelvinToCelsius(data.main.temp).toString()
+            _currentTemperature.value = round(kelvinToCelsius(data.main.temp)).toInt().toString() + " °C"
+            _todayMaxTemp.value = kelvinToCelsius(data.main.temp_max).toString()
+            _todayMinTemp.value = kelvinToCelsius(data.main.temp_min).toString()
         }
     }
 
@@ -102,5 +150,9 @@ class HomeFragmentModel : ViewModel() {
             "50d", "50n" -> R.drawable.foggy
             else -> R.drawable.unknown
         }
+    }
+
+    fun getWeatherDataValue(): LiveData<WeatherData?> {
+        return weatherData
     }
 }
--- Projekt/app/src/main/java/com/example/aplikacijazaprognozuvremena/network/WeatherApiService.kt
+++ Projekt/app/src/main/java/com/example/aplikacijazaprognozuvremena/network/WeatherApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val BASE_URL = "https://api.openweathermap.org/"
+private const val BASE_URL = "https://api.openweathermap.org"
 private const val API_KEY = "f068ce460229a68c927391f4330c17a3"
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -20,7 +20,7 @@ interface WeatherApiService {
     @GET("data/2.5/weather")
     suspend fun getWeather (
         @Query("q") city: String = "London,uk",
-        @Query("APPID") apiKey: String = API_KEY ): WeatherData
+        @Query("appid") apiKey: String = API_KEY ): WeatherData
 }
 
 object WeatherApi {
